3090355bb9dbdb4a718cd9642276da15b3929655
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-10-22 05:08:18 -0700

    added source

--- webscraper.py
+++ webscraper.py
@@ -11,10 +11,6 @@ soup = BeautifulSoup(page.content, "lxml")
 results = soup.find(id="search-results")
 cl_jobs = results.find_all("li", class_="result-row") # for main loop
 
-_href_url = []
-_time = []
-_job_location = []
-
 file = open('cl-jobs.csv','w')
 writer = csv.writer(file)
 
@@ -26,14 +22,11 @@ time = []
 job_location = []
 for cl_job in cl_jobs:
     
-    #for i in soup.findAll('li'):
-    #    print(i.text.strip())
+    href_url.append(soup.findAll(class_="result-title")[0].text.strip())
 
-    href_url.append = soup.findAll(class_="result-title")[0].text.strip()
+    time.append(soup.findAll(class_="result-date")[0].text.strip()) # 12:23
 
-    time.append = soup.find(class_="result-date")[0].text.strip() # 12:23
-
-    job_location.append = soup.find(class_="result-hood")[0].text.strip() # location (city)
+    job_location.append(soup.findAll(class_="result-hood")[0].text.strip()) # location (city)
 
     writer.writerow([href_url, time, job_location])
 
