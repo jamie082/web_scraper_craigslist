5f3f19457316d6421cc17af2e2434720779dfbfc
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-10-22 04:36:05 -0700

    added source

--- webscraper.py
+++ webscraper.py
@@ -6,10 +6,14 @@ import csv
 
 URL = "https://sandiego.craigslist.org/search/sof"
 page = requests.get(URL)
-soup = BeautifulSoup(page.content, "html.parser")
+soup = BeautifulSoup(page.content, "lxml")
 
 results = soup.find(id="search-results")
-cl_jobs = results.find(class_="result-row") # for main loop
+cl_jobs = results.find_all("li", class_="result-row") # for main loop
+
+_href_url = []
+_time = []
+_job_location = []
 
 file = open('cl-jobs.csv','w')
 writer = csv.writer(file)
@@ -18,10 +22,12 @@ writer = csv.writer(file)
 writer.writerow(["Title", "Date", "Location"])
 
 for cl_job in cl_jobs:
+    
+    for i in soup.findAll("h3"):
+        print(i.text.strip())
 
     href_url = soup.find(class_="result-title").text.strip()
 
-    # third entry
     time = soup.find(class_="result-date").text.strip() # 12:23
 
     job_location = soup.find(class_="result-hood").text.strip() # location (city)
