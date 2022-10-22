3e7c642a84036768bb9922949be859db73104d79
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-10-22 05:30:08 -0700

    added working code

--- webscraper.py
+++ webscraper.py
@@ -22,11 +22,11 @@ time = []
 job_location = []
 for cl_job in cl_jobs:
     
-    href_url.append(soup.findAll(class_="result-title")[0].text.strip())
+    href_url = soup.find(class_="result-title").text.strip()
 
-    time.append(soup.findAll(class_="result-date")[0].text.strip()) # 12:23
+    time = soup.find(class_="result-date").text.strip() # 12:23
 
-    job_location.append(soup.findAll(class_="result-hood")[0].text.strip()) # location (city)
+    job_location = soup.find(class_="result-hood").text.strip() # location (city)
 
     writer.writerow([href_url, time, job_location])
 
